14c29edad588be2db827d02c94cab33c95cad4d4
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-12 12:30:13 +0200

    updated landingpage.ts

--- src/app/isoko-landing-page/isoko-landing-page.ts
+++ src/app/isoko-landing-page/isoko-landing-page.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener} from '@angular/core';
+import { NgClass } from "../../../node_modules/@angular/common/types/_common_module-chunk";
+import { LucideAngularModule } from 'lucide-angular';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-isoko-landing-page',
-  imports: [],
+  imports: [NgClass, CommonModule, LucideAngularModule],
   templateUrl: './isoko-landing-page.html',
   styleUrl: './isoko-landing-page.css',
 })
 export class IsokoLandingPage {
   isMenuOpen = false;
   scrolled = false;
+
+  @HostListener("window:scroll", []) onWindowScroll() {
+    this.scrolled = window.scrollY >50;
+  }
+
+  toggleMenu () {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
