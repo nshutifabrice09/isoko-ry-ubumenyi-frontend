cf29602cabdedc55c0009eb8a37b737b9c1707cb
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-12 23:14:31 +0200

    updates app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { IsokoLandingPageComponent } from './isoko-landing-page/isoko-landing-page';
+import { Register } from './auth/register/register';
+import { Login } from './auth/login/login';
 
 export const routes: Routes = [
     { path: '', component: IsokoLandingPageComponent},
+    { path: '', component: Register},
+    { path: '', component: Login},
+    { path: '**', redirectTo: ''}
 ];
