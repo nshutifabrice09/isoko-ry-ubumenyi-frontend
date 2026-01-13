aaa316fca4e0d6815cce4b5a9919ebd36c044768
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-13 17:55:50 +0200

    initial contactComponent commit

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterLink],
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
