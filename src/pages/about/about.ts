d298adf08193e3996f0f3829c4777519e0b98c28
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-13 17:34:18 +0200

    aboutComponents updates and app.routers.ts

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  standalone: true,
+  imports: [ CommonModule, RouterLink],
   templateUrl: './about.html',
   styleUrl: './about.css',
 })
+
+
 export class About {
 
 }
+
+
