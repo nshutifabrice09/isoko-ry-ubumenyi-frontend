8e17e2c309d6001b421927611bf2c84bee113efc
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-13 10:01:31 +0200

    initial about components commit

---
+++ src/pages/instructors/instructors.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-instructors',
+  imports: [],
+  templateUrl: './instructors.html',
+  styleUrl: './instructors.css',
+})
+export class Instructors {
+
+}
