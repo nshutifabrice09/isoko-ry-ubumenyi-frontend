3a8df2620774263a4b1efb230ee2aa159ac5a6fb
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-15 20:15:05 +0200

    updated on authInterceptor commit

--- src/app/interceptors/auth-interceptor.ts
+++ src/app/interceptors/auth-interceptor.ts
@@ -1,5 +1,18 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Auth } from '../services/auth';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   return next(req);
+
+  
 };
