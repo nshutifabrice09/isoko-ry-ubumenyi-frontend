b6595b662b9567f8a4803fc4679de6e815299f8b
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-15 18:25:07 +0200

    final userService commit

--- src/app/services/user.ts
+++ src/app/services/user.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class User {
+  export class UserService {
+  private apiUrl = 'http://localhost:8080/api/users';
+
+  constructor(private http: HttpClient) {}
+
+  // Get user profile
+  getUserProfile(userId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${userId}`);
+  }
+
+  // Update user profile
+  updateUserProfile(userId: number, userData: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${userId}`, userData);
+  }
+
+  // Get all users (admin only)
+  getAllUsers(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+}
   
 }
