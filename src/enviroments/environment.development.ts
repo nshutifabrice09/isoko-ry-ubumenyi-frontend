fa0801f7ecb4d5ba4702e039cc2ecb97fed51f77
nshuti.fabrice <nshuti.fabrice09@gmail.com>
2026-01-19 11:14:06 +0200

    updates on environment files

--- src/enviroments/environment.development.ts
+++ src/enviroments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'  // Your Spring Boot backend URL
+  apiUrl: 'http://api.isokoryubumenyi.com/api'  // Your Spring Boot backend URL
 };
